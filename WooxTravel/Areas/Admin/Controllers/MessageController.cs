fdbe14d22d818a7a20c28db7da9c2c6e18b0e6b1
Emre <112270755+emrrekus@users.noreply.github.com>
2024-10-23 21:48:09 +0300

    Mesaj kısmı eklendi

--- WooxTravel/Areas/Admin/Controllers/MessageController.cs
+++ WooxTravel/Areas/Admin/Controllers/MessageController.cs
@@ -13,7 +13,40 @@ namespace WooxTravel.Areas.Admin.Controllers
         TravelContext context = new TravelContext();
         public ActionResult Inbox()
         {
+            var a = Session["x"];
+            var email = context.Admins.Where(x => x.Username == a).Select(x => x.Email).FirstOrDefault();
+            var values = context.Messages.Where(x=>x.ReceiverMail==email).ToList();
+
+            return View(values);
+        }
+
+
+        public ActionResult SendBox()
+        {
+            var a = Session["x"];
+            var email = context.Admins.Where(x => x.Username == a).Select(x => x.Email).FirstOrDefault();
+            var values = context.Messages.Where(x => x.SenderMail == email).ToList();
+
+            return View(values);
+        }
+
+        public ActionResult SendMessage()
+        {
             return View();
         }
+        [HttpPost]
+        public ActionResult SendMessage(Message message)
+        {
+            var a = Session["x"];
+            var email = context.Admins.Where(x => x.Username == a).Select(x => x.Email).FirstOrDefault();
+            message.SenderMail = email;
+            message.SendDate = DateTime.Now;
+            message.IsRead =false;
+            context.Messages.Add(message);
+            context.SaveChanges();
+            return RedirectToAction("SendBox", "Message", new {area="Admin"});
+        }
     }
+
+   
 }
